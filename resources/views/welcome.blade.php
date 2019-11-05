3f1b76c3300ade0992d015877aa21fba31d95d6d
win-chanma <t.ueki.dev@gmail.com>
2019-11-05 17:27:40 +0900

    Automatic Deploysの機能テスト

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel - depoly from github -
+                    Laravel - Automatic Deploys -
                 </div>
 
             </div>
