03ffa6a039375fd2ce90e448c68f6f9f029358cb
win-chanma <t.ueki.dev@gmail.com>
2019-11-12 17:51:35 +0900

    サービスプロバイダの登録

--- config/app.php
+++ config/app.php
@@ -177,6 +177,10 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        /*
+         * Other Service Providers
+         */
+        App\Providers\RiakServiceProvider::class,
     ],
 
     /*
