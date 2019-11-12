6aa52f268a9e67a5fbff0fd7d22a44ff8fa08316
win-chanma <t.ueki.dev@gmail.com>
2019-11-12 17:37:49 +0900

    サービスコンテナへの結合登録

---
+++ app/Providers/RiakServiceProvider.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Database\Connection;
+use Illuminate\Support\ServiceProvider;
+
+class RiakServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        // コンテナへの結合登録
+        // Singletonで登録なので、他の場所から呼ばれた場合でも同一インスタンスが呼ばれる
+        $this->app->singleton(Connetion::class,function ($app){
+            return new Connection(config('riak'));
+        });
+    }
+}
